a7949d595fa6e8b26014322099454c01b9068c53
Pierpaolo Giorgi <pierpaologiorgiiccs@gmail.com>
2024-01-15 09:22:51 +0100

    Modifica

--- esercizio-settimana-7/src/dati/dati.js
+++ esercizio-settimana-7/src/dati/dati.js
@@ -1,2 +1,2 @@
 export const authorization = 'a73641d4';
-export const urlApi = 'http://www.omdbapi.com/?i=tt3896198&apikey=a73641d4&s=Pirates';
+export const urlApi = 'http://www.omdbapi.com/?i=tt3896198&apikey=a73641d4&s=Banana';
